ef46a0f53751d9344cee22aaa2aff9cd47af7e5c
kibetchumo-svg <kibet.chumo@student.moringaschool.com>
2025-09-09 10:54:16 +0300

    arithmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
-
-// Write your code here
+let num1 = 31;
+let num2 =2;
+let multiply = num1*num2;
+console.log(multiply);
+// Create a variable called random that will generate a random integer greater than 0.
+let random= Math.floor(Math.random()*100) +1;
+console.log(random);
+/* modulus */
+let num3 = 16;
+let num4 = 6;
+let mod = num3 % num4;
+console.log(mod);
+/* Create a variable called max that finds the highest number in a set; value returned should be 20.
+ */
+let max = Math.max(5, 13, 20, 9);
+console.log(max);
